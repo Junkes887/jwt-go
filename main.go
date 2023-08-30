85c8eb9ba2e261822ce56cfba0dcf67102167ea9
Junkes887 <henriquejunkes887@gmail.com>
2023-08-29 21:04:50 -0300

    relase-0.0.1

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var secretKey = []byte("seu-secret-key-aqui")
+
+func main() {
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/protected", protectedHandler)
+
+	fmt.Println("Servidor rodando na porta 8080...")
+	http.ListenAndServe(":8080", nil)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	// Verifique as credenciais (normalmente contra um banco de dados)
+	username := "usuario"
+	password := "senha"
+
+	// Simples exemplo de autenticação
+	if r.FormValue("username") == username && r.FormValue("password") == password {
+		// Crie um token JWT
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"username": username,
+			"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+		tokenString, err := token.SignedString(secretKey)
+		if err != nil {
+			http.Error(w, "Erro ao gerar token", http.StatusInternalServerError)
+			return
+		}
+
+		w.Write([]byte(tokenString))
+	} else {
+		http.Error(w, "Credenciais inválidas", http.StatusUnauthorized)
+	}
+}
+
+func protectedHandler(w http.ResponseWriter, r *http.Request) {
+	tokenString := r.Header.Get("Authorization")
+	if tokenString == "" {
+		http.Error(w, "Token ausente", http.StatusUnauthorized)
+		return
+	}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		http.Error(w, "Token inválido", http.StatusUnauthorized)
+		return
+	}
+
+	if token.Valid {
+		w.Write([]byte("Acesso permitido a rota protegida!"))
+	} else {
+		http.Error(w, "Token inválido", http.StatusUnauthorized)
+	}
+}
